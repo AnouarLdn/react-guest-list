05f8b963d7b87ad093f2e58c3e750526122959e1
Anouar <anouar.haidary@gmail.com>
2021-09-14 17:14:25 +0200

    Guest list app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import stylesheet from "./App.css";
+
+function App() {
+
+//useState
+const [person, setPerson] = useState([]);
+
+//functions
+
+//Add element
+const addPerson = () => {
+    const firstName = document.querySelector(".content").value;
+    if (firstName) {
+    setPerson(prevName => [...prevName, firstName]);
+    console.log(firstName);
+    document.querySelector(".content").value = "";
+    }
+}
+
+//Delete element
+const deletePerson = (e) => {
+    const { id } = e.currentTarget;
+    person.splice(id, 1)
+    setPerson([...person]);
+    console.log("person deleted");
+}
+
+// JSX
+    return(
+    <div className="mainContainer">
+        <h1>GUEST LIST</h1>
+        <br/>
+        <input className="content"></input>
+        <br/>
+        <button onClick={addPerson}>Add guest</button>
+        <ul>
+            {person.map((pips,i) =>
+                <li key={person.id} className="listItem">
+                    {pips}
+                    <span> </span><button key={person.id} id={i} onClick={deletePerson} className="buttonListItem"> x </button>
+                </li>
+                )}
+        </ul>
+    </div>
+)
+}
+export default App;
